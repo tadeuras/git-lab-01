3d15c700cf103ce539e85c27b781eb061afc4ae3
Tadeu Santos <tadeu.santos@accenture.com>
2018-10-05 15:06:32 -0300

    mensagem de hello duplicada

--- hello-world/Program.cs
+++ hello-world/Program.cs
@@ -6,7 +6,8 @@ namespace hello_world
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("1 - Hello World!");
+            Console.WriteLine("2 - Hello World!");
         }
     }
 }
