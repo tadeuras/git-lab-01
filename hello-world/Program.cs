00c724bb10240b2b1de72480461d373ebaea4c93
Tadeu Santos <tadeu.santos@accenture.com>
2018-10-05 15:19:02 -0300

    linhas 2 e 3 corrigidas

--- hello-world/Program.cs
+++ hello-world/Program.cs
@@ -7,8 +7,8 @@ namespace hello_world
         static void Main(string[] args)
         {
             Console.WriteLine("1 - Hello World!");
-            Console.WriteLine("2 - Hello World!");
-            Console.WriteLine("3 - Hello World!");
+            Console.WriteLine("2 : Hello World!");
+            Console.WriteLine("3 : Hello World!");
         }
     }
 }
